19ad7ce399d429e1ce88d109b12773a797c227a5
Eyder Rios <eyder.rios@gmail.com>
2026-03-17 12:02:52 -0300

    Aula 2026-03-17

---
+++ unit2/test/validate-cpf.spec.ts
@@ -0,0 +1,31 @@
+import { validateCpf } from '@src/validate-cpf'
+
+describe('validateCpf', () => {
+  const validCpfs = ['177.594.490-54', '515070430-07', '78437331080']
+  const invalidCpfs = ['12345678900', '1234567890', '123456789000']
+  const nullOrEmptyCpfs = [null, undefined, '', ' ', '   ']
+
+  test.each(validCpfs)('Should validate a valid CPF: "%s"', (validCpf: string) => {
+    // Arrange
+    // Act
+    const r = validateCpf(validCpf)
+    // Assert
+    expect(r).toBe(true)
+  })
+
+  test.each(nullOrEmptyCpfs)('Should not validate a null or empty CPF: "%s"', (nullOrEmptyCpf: unknown) => {
+    // Arrange
+    // Act
+    const r = validateCpf(nullOrEmptyCpf as string)
+    // Assert
+    expect(r).toBe(false)
+  })
+
+  test.each(invalidCpfs)('Should not validate an invalid CPF: "%s"', (invalidCpf: string) => {
+    // Arrange
+    // Act
+    const r = validateCpf(invalidCpf)
+    // Assert
+    expect(r).toBe(false)
+  })
+})
